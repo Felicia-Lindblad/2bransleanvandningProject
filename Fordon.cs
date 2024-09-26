9acf34ea1a68db078399dc98b0ae3d43e3ad1352
Felicia-Lindblad <lindblad.felicia@gmail.com>
2024-09-26 20:17:56 +0200

    Add project files.

---
+++ Fordon.cs
@@ -0,0 +1,23 @@
+﻿namespace _2bränsleanvändningProject
+{
+    public class Fordon
+    {
+        public int miltal;
+        public int bränsleVolym;
+
+        public virtual int CalculateFuelEfficiensy()
+        {
+            return bränsleVolym / miltal;
+        }
+    }
+
+    public class ElectricCar : Fordon
+    {
+        public int batteri;
+        
+        public override int CalculateFuelEfficiensy()
+        {
+            return bränsleVolym / miltal + batteri;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _2bränsleanvändningProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please enter your fuelrange in Liter");
+            int fuelRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please eneter your droven miles");
+            int milesDroven = Convert.ToInt32(Console.ReadLine());
+
+            Fordon fordon = new Fordon();
+
+            fordon.miltal = milesDroven;
+            fordon.bränsleVolym = fuelRange;
+
+            int efficiency = fordon.CalculateFuelEfficiensy();
+            Console.WriteLine($"Your fuelefficiency is {efficiency}");
+
+            Console.WriteLine("Please enter your fuelrange in Liter");
+            int fuelRangeCar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please eneter your droven miles");
+            int milesDrovenCar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please eneter your batteryrange in miles");
+            int batteryMiles = Convert.ToInt32(Console.ReadLine());
+
+            ElectricCar car = new ElectricCar();
+
+            car.miltal = milesDrovenCar;
+            car.bränsleVolym = fuelRangeCar;
+            car.batteri = batteryMiles;
+
+            int efficiencyCar = car.CalculateFuelEfficiensy();
+            Console.WriteLine($"Your fuel efficiency for car is {efficiencyCar}");
+
+
+
+        }
+    }
+}
